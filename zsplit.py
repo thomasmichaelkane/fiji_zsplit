cca56e6d2757ffd24572c296b9cdea5c2b0504ab
tktk <thomas.kane.ucl@gmail.com>
2024-05-15 11:55:46 +0100

    simplified code as input images now saved as a hyperstack

--- zsplit.py
+++ zsplit.py
@@ -5,50 +5,32 @@ import tifffile
 
 def run():
     
-    path, frames_per_acq = parse_args()
+    path = parse_args()
     
-    stack, total_frames = read_stack(path, frames_per_acq)
-    
-    zprojs = split_and_zproj(stack, frames_per_acq, total_frames)
+    zprojs = split_and_zproj(path)
     
     save_zprojs(zprojs, path)
 
 ################################################################
 
-def read_stack(path, frames_per_acq):
-
-    tiff_stack = tifffile.imread(path)
-
-    total_frames = tiff_stack.shape[0]
-
-    if total_frames % frames_per_acq != 0:
-        raise ValueError("The total frame number of the stack but be divisible by the frames per acqisition")
-    else:
-        num_new_stacks = int(total_frames / frames_per_acq)
-        log = f"Splitting {total_frames} frames length tiff stack into {num_new_stacks} Z-projections, with {frames_per_acq} frames per projection"
-        print(log)
-        
-    return tiff_stack, total_frames
-
-def split_and_zproj(tiff_stack, frames_per_acq, total_frames):
+def split_and_zproj(path):
     
-    acqs = [tiff_stack[i:i+frames_per_acq, :, :] for i in range(0, total_frames, frames_per_acq)]
-    zprojs = [np.mean(acq, 0) for acq in acqs]
-    
+    stack = tifffile.imread(path)
+
+    log = f"Number of images: {stack.shape[0]} | Number of frames to average: {stack.shape[1]}"
+    print(log)
+
+    zprojs = np.mean(stack, 1)
+
     return zprojs
 
 def save_zprojs(zprojs, path):
     
     folder, orig_name = os.path.split(path)
     basename, _ = os.path.splitext(orig_name)
-    
-    output_folder = os.path.join(folder, "z_projections")
-    os.mkdir(output_folder)
-    
-    for i, zproj in enumerate(zprojs):
-        name = basename + "_" + str(i) + ".tif"
-        zproj_path = os.path.join(output_folder, name)
-        tifffile.imwrite(zproj_path, zproj)
+    zproj_name = basename +  "_Z-projections.tif"
+    zproj_path = os.path.join(folder, zproj_name)
+    tifffile.imwrite(zproj_path, zprojs)
 
 def parse_tiff(arg):
 
@@ -63,33 +45,21 @@ def parse_tiff(arg):
     else:
         raise FileNotFoundError
 
-def parse_frames_per_acq(arg):
-    
-    try:
-        
-        arg = int(arg)
-        return arg
-            
-    except ValueError:
-        
-        raise NameError("frames per acquisition needs to be an integer")
-
 def parse_args():
 
     if len(sys.argv) == 1:
         
         raise KeyError("No tiff specified")
-    
+        
     if len(sys.argv) == 2:
         
-        raise KeyError("Frames per acquisition not set")
-        
-    if len(sys.argv) == 3:
-        
         path = parse_tiff(sys.argv[1])
-        frames_per_acq = parse_frames_per_acq(sys.argv[2])
+
+    if len(sys.argv) >= 3:
+
+        raise KeyError("Too many arguments")
         
-    return path, frames_per_acq
+    return path
 
 if __name__ == "__main__":
     
